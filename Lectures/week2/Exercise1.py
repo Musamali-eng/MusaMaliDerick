481925404f8d90863163fe2a6ea914730625b16f
Musa Mali Derick <musamalidrick203@gmail.com>
2026-06-16 23:28:53 +0300

    functions

---
+++ Lectures/week2/Exercise1.py
@@ -0,0 +1,7 @@
+def rectangle_area():
+    length = int(input("Enter the length: "))
+    width = int(input("Enter the width: "))
+    area = length * width
+    print(f"The area of the rectangle is {area}")
+
+rectangle_area()
--- Lectures/week2/function.py
+++ Lectures/week2/function.py
@@ -1,1 +1,50 @@
-de
+#functions in python
+#built in functions
+#Example of built in functions
+#print("Hello world!") #This will print Hello world! to the console
+#len("Hello world!") # This will return the length of the string "Hello world!" which is 12
+
+#def greet():
+ #print("Welcome to Python programming!")
+#Calling a function     
+#greet() #This will call the greet and print welcome to the python programming
+#greet() #You can call the function multiple times to reuse the code
+
+
+#LabAcitivity2:Mathematical function
+#square of number num = 5
+#def square_number():
+   # num = 5
+    #square  = num * num
+   # print(square)
+#calling the function
+#square_number()
+
+#parameters
+#def greet_user(name):
+ # print(f"My name is", name)
+#greet_user("mak")
+
+
+#arguments
+#example
+# greet ("mark john")
+
+# Fuction with a single parameter
+#def greet(name):
+ # print(f"My name is", name)
+#greet("Mukisa Mark")
+
+#multiple parameter
+#def add_numbers(a,b):
+   # print(a+b)
+
+#add_numbers(10,20)
+
+#LabActivity
+#positional arguments
+def display(name,age):
+  print(name)
+  print(age)
+display("Juma Sadamu",30)
+
