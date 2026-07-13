bcf20a00e448196b189b0ccaf05049eaf1ebda7b
Musa Mali Derick <musamalidrick203@gmail.com>
2026-07-13 13:54:23 +0300

    learning django in week 6

---
+++ Lectures/week6/morning/Djangoo.py
@@ -0,0 +1,13 @@
+#Why the virtual environment in python
+#Is an isolated workspace that contains its own python interpreter and installed package
+#Activate virtual environment
+#source .venv/Scripts/activate
+
+
+#MVT which stands for model-view-Template design style
+#1 model:represents the data you want to display
+#2 View: Handles incoming requests and return the appropriate
+#3 Template: Atext file usually HTML that defines the structure
+
+#How to create a Django project
+#Use django-admin startproject name
